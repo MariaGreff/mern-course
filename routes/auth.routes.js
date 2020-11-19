54fa2570f46a4ffd03555f055559afedc16ad364
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-11-19 15:03:16 +0100

    feat: created auth route, implemented jwt token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
-const {check, validationResult} = require('express-validator')
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = Router();
 
@@ -40,8 +42,45 @@ router.post('/register', [
 });
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post('/login', [
+    check('email', 'Enter correct email').normalizeEmail().isEmail(),
+    check('password', 'Enter password').exists()
+] , async (req, res) => {
+    try {
+        const errors = validationResult(req);
 
+        if (errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Incorrect data while logging'
+            })
+        }
+
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: 'User is not found'})
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Wrong password, try again' });
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: '1h' }
+        );
+
+        res.json({ token, userId: user.id });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong, try again' });
+    }
 });
 
 module.exports = router;
