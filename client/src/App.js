b0af4ea0f0342606a5bc94d38c88ca54f75835a2
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-11-23 15:58:06 +0100

    feat: created auth hook

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import 'materialize-css';
 import {useRoutes} from './routes';
+import { useAuth } from './hooks/auth.hook';
 
 function App() {
+  const {token, login, logout, userId } = useAuth();
   const routes = useRoutes(false);
   return (
     <Router>
