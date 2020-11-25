651c2665ee8da0cd37de566931178e8fc0588b9f
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-11-25 14:59:45 +0100

    feat: added scripts as preparation for uploading to hosting

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const config = require('config');
+const path = require('path');
 const mongoose = require('mongoose');
 
 
@@ -10,6 +12,14 @@ app.use(express.json({ extended: true}));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = config.get('port') || 5000;
 
@@ -23,7 +33,6 @@ async function start() {
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
     } catch (error) {
         console.log('Server error', error.message);
-        // eslint-disable-next-line no-undef
         process.exit(1);
     }
 }
